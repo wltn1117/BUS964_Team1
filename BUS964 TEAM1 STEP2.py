c9de38e08fbb43212233e03694853447af543752
wltn1117 <wltn1117@gmail.com>
2026-04-09 17:27:14 +0900

    STEP2 코드
    STEP2 코드

--- BUS964 TEAM1 STEP2.py
+++ BUS964 TEAM1 STEP2.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
+ 
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
